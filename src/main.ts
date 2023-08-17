ea0cf857c603bac2d68970789c73bad815bfa327
karim <karim.sekri@gmail.com>
2023-08-17 12:35:19 +0200

    Debut Ajout style couleur

--- src/main.ts
+++ src/main.ts
@@ -22,8 +22,9 @@ function afficher_debut_jeu (){
     tile.setAttribute("class", "tile")
     tile.style.width ="100px"
     tile.style.height = "100px"
-    tile.style.backgroundColor = colors[Math.floor(i/2)]
+    //tile.style.backgroundColor = colors[Math.floor(i/2)]
     tile.style.borderRadius = "5px";
+    tile.classList.add(colors[Math.floor(i/2)])
     return tile
 })
 
@@ -31,6 +32,15 @@ tiles.sort(() => Math.random() - 0.5);
 // Add the tiles to the app
 tiles.forEach( tile => divContainer.appendChild(tile))
 
+
+let nodeList = document.querySelectorAll(".tile");
+let elements = Array.from(nodeList);
+elements.forEach( (element) => {
+        element.classList.add("not-revealed")
+        element.addEventListener("click", () => {
+            element.classList.remove("not-revealed")
+        })
+})
 
 }
 
