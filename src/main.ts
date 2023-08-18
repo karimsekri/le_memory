0c916f284a7113ab27961c905a6c7e1986d9da58
karim <karim.sekri@gmail.com>
2023-08-18 14:59:48 +0200

    Ajout de 8 images doublées

--- src/main.ts
+++ src/main.ts
@@ -7,19 +7,20 @@ const colors = ["red", "blue", "green", "yellow", "orange", "purple", "pink", "b
 let compteur = 0 ;
 let i = 0;
 let lastcolor: HTMLDivElement | null= null;
-let secondColor : string;
+let lastImage : string;
 
-// Promises
-fetch("https://dog.ceo/api/breeds/image/random")
-.then((response) => {
-    return response.json();
-})
-.then((data) => {
-    console.log(data);
-    notreImage.setAttribute("src", data.message)
+
+// // Promises
+// fetch("https://dog.ceo/api/breeds/image/random")
+// .then((response) => {
+//     return response.json();
+// })
+// .then((data) => {
+//     console.log(data);
+//     notreImage.setAttribute("src", data.message)
     
-})
-app.appendChild(notreImage);
+// })
+// app.appendChild(notreImage);
 btnStart.addEventListener("click", () => {
   labelGagne.remove();
   afficher_debut_jeu();
@@ -42,16 +43,34 @@ function afficher_debut_jeu (){
   
 
   const tiles = new Array(16).fill('').map( (_, i) => {
-    const tile = document.createElement("div")
+    const tile = document.createElement("img")
     tile.setAttribute("class", "tile")
     tile.style.width ="100px"
     tile.style.height = "100px"
     //tile.style.backgroundColor = colors[Math.floor(i/2)]
     tile.style.borderRadius = "5px";
     tile.classList.add(colors[Math.floor(i/2)])
-    tile.setAttribute("color",colors[Math.floor(i/2)])
+    // tile.setAttribute("color",colors[Math.floor(i/2)])
     
-    return tile
+    // Promises
+    if (i%2 == 0) {
+      
+    
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        //console.log(data);
+        tile.setAttribute("src", data.message);
+        lastImage = data.message;
+        console.log(data.message);
+        console.log(lastImage);
+    })
+  }
+  
+    
+  return tile
   })
 
   tiles.sort(() => Math.random() - 0.5);
