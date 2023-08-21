80c6a9d35e29ab02ee46f6a7b9ff1bc3bdc350f9
karim <karim.sekri@gmail.com>
2023-08-21 10:36:51 +0200

    Affichage des images

--- src/main.ts
+++ src/main.ts
@@ -2,25 +2,22 @@ const app = document.querySelector('#app') as HTMLDivElement;
 const btnStart = document.querySelector("#init-button") as HTMLButtonElement;
 const labelGagne = document.createElement("label") as HTMLLabelElement;
 const notreImage = document.createElement("img") as HTMLImageElement;
-const colors = ["red", "blue", "green", "yellow", "orange", "purple", "pink", "brown"];
+//const colors = ["red", "blue", "green", "yellow", "orange", "purple", "pink", "brown"];
 
 
 let compteur = 0 ;
 let i = 0;
-let lastcolor: HTMLDivElement | null= null;
-let lastImage = [""];
-lastImage.shift();
+let lastcolor: HTMLImageElement | null= null;
+let lastImage: string[] = [];
 
 for (let index = 0; index < 8; index++) {
-  
-    fetch("https://dog.ceo/api/breeds/image/random")
+  fetch("https://dog.ceo/api/breeds/image/random")
     .then((response) => {
         return response.json();
     })
     .then((data) => {
-       // console.log(data);
-        lastImage.push(data.message);
-        console.log(lastImage);
+       
+        lastImage.push(data.message);       
         
     })
 }
@@ -28,10 +25,7 @@ for (let index = 0; index < 8; index++) {
 btnStart.addEventListener("click", () => {
   labelGagne.remove();
   afficher_debut_jeu();
-  
-  
 });
-
 
 
 
@@ -47,44 +41,53 @@ function afficher_debut_jeu (){
   
 
   const tiles = new Array(16).fill('').map( (_, i) => {
+    const tileDiv = document.createElement("div")
+    tileDiv.setAttribute("class", "tileDiv")
+
     const tile = document.createElement("img")
     tile.setAttribute("class", "tile")
     tile.style.width ="100px"
     tile.style.height = "100px"
     //tile.style.backgroundColor = colors[Math.floor(i/2)]
-    tile.style.borderRadius = "5px";   
-    tile.setAttribute("src",lastImage[Math.floor(i/2)])
+    tile.style.borderRadius = "5px";  
+    tile.setAttribute('index', Math.floor(i/2).toString()) 
+    tile.setAttribute("src",lastImage[0])
+    tileDiv.appendChild(tile)
   
-  return tile
+  return tileDiv
   })
 
   tiles.sort(() => Math.random() - 0.5);
   // Add the tiles to the app
-  tiles.forEach( tile => divContainer.appendChild(tile))
+  tiles.forEach( tileDiv => divContainer.appendChild(tileDiv))
+  
 
 
 let nodeList = document.querySelectorAll(".tile");
-let elements = Array.from(nodeList) as HTMLDivElement[];
+let elements = Array.from(nodeList) as HTMLImageElement[];
 elements.forEach( (element) => {
-        element.classList.add("not-revealed")
+        console.log(element);
+               
         element.addEventListener("click", () => {
          
-          compteur++;
-          element.classList.remove("not-revealed")
+          compteur++;                 
+          element.setAttribute("src",lastImage[parseInt(element.getAttribute("index") as string)])
+         
 
           if(lastcolor === null){
             lastcolor = element
           }
           else {
-            if(lastcolor.getAttribute('color') !== element.getAttribute("color")){
+            if(lastcolor.getAttribute('src') !== element.getAttribute("src")){
               setTimeout(() => {
-                element.classList.add("not-revealed")
-                lastcolor?.classList.add("not-revealed")
+                element.removeAttribute("src")
+
+                lastcolor?.removeAttribute("src")
                 lastcolor = null
               }, 1000)
             }
           else{
-            if(lastcolor.getAttribute('color') === element.getAttribute("color")){
+            if(lastcolor.getAttribute('src') === element.getAttribute("src")){
                 lastcolor = null;  
                 i++;
                 if (i === 8 ) {
